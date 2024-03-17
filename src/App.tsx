7525b517228dca3bb7092fd78a51b0f1b04ee116
Eduado-DevJs <dudev2001@gmail.com>
2024-03-17 11:50:58 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useRef } from "react";
+import Video from "./assets/video.mp4";
+
+export default function App() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  async function modePicture() {
+    if(!videoRef.current) return;
+    if(document.pictureInPictureElement){
+      await document.exitPictureInPicture()
+    }else {
+      await videoRef.current.requestPictureInPicture()
+    }
+  }
+
+  return (
+    <div style={{ maxWidth: "1200px", margin: "0 auto", padding: "20px" }}>
+      <video
+        ref={videoRef}
+        style={{ borderRadius: "20px" }}
+        src={Video}
+      ></video>
+      <button onClick={modePicture}>PIP</button>
+    </div>
+  );
+}
